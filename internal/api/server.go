f31a20324da044139773e55b7e1c654862e7a84b
Airlangga <airlanggakrishna@gmail.com>
2025-11-17 22:23:28 +0700

    add setupRoutes in server.go

--- internal/api/server.go
+++ internal/api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/airlangga-hub/ecommerce-go/config"
+	"github.com/airlangga-hub/ecommerce-go/internal/api/rest"
+	"github.com/airlangga-hub/ecommerce-go/internal/api/rest/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -9,5 +11,16 @@ import (
 func StartServer(cfg config.AppConfig) {
 	app := fiber.New()
 
+	httpHandler := rest.HttpHandler{
+		App: app,
+	}
+
+	setupRoutes(&httpHandler)
+
 	app.Listen(cfg.ServerPort)
+}
+
+
+func setupRoutes(rh *rest.HttpHandler) {
+	handlers.SetupUserRoutes(rh)
 }
