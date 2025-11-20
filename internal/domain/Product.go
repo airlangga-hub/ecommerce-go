2f3c8d12379011d324bdbe2471b03f4c2c7872f8
Airlangga <airlanggakrishna@gmail.com>
2025-11-21 03:02:32 +0700

    add Product.go and update Category.go

--- internal/domain/Product.go
+++ internal/domain/Product.go
@@ -3,12 +3,15 @@ package domain
 import "time"
 
 
-type Category struct {
+type Product struct {
 	ID 				uint 		`json:"id" gorm:"PrimaryKey"`
 	Name 			string 		`json:"name" gorm:"index"`
-	ParentID 		uint 		`json:"parent_id"`
-	Products 		[]Product 	`json:"products"`
-	DisplayOrder 	int 		`json:"display_order"`
+	Description 	string 		`json:"description"`
+	Price			float64		`json:"price"`
+	Stock			uint		`json:"stock"`
+	ImageURL 		string 		`json:"image_url"`
+	UserID 			uint 		`json:"user_id"`
+	CategoryID	 	uint 		`json:"category_id"`
 	CreatedAt		time.Time	`json:"created_at" gorm:"default:current_timestamp"`
 	UpdatedAt		time.Time	`json:"updated_at" gorm:"default:current_timestamp"`
 }
