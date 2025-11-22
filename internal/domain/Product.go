aad2d7ee19559c45824fe2eef0b042a5b53406c5
Airlangga <airlanggakrishna@gmail.com>
2025-11-23 02:25:19 +0700

    add foreign key explicitly

--- internal/domain/Product.go
+++ internal/domain/Product.go
@@ -2,16 +2,18 @@ package domain
 
 import "time"
 
-
 type Product struct {
-	ID 				uint 		`json:"id" gorm:"PrimaryKey"`
-	Name 			string 		`json:"name" gorm:"index"`
-	Description 	string 		`json:"description"`
-	Price			float64		`json:"price"`
-	Stock			uint		`json:"stock"`
-	ImageURL 		string 		`json:"image_url"`
-	UserID 			uint 		`json:"user_id"`
-	CategoryID	 	uint 		`json:"category_id"`
-	CreatedAt		time.Time	`json:"created_at" gorm:"default:current_timestamp"`
-	UpdatedAt		time.Time	`json:"updated_at" gorm:"default:current_timestamp"`
+	ID          uint      	`json:"id" gorm:"primaryKey"`
+	Name        string    	`json:"name" gorm:"index"`
+	Description string    	`json:"description"`
+	Price       float64   	`json:"price"`
+	Stock       uint      	`json:"stock"`
+	ImageURL    string    	`json:"image_url"`
+	UserID      uint      	`json:"user_id"`
+	CategoryID  uint      	`json:"category_id"`
+	CreatedAt   time.Time 	`json:"created_at" gorm:"default:current_timestamp"`
+	UpdatedAt   time.Time 	`json:"updated_at" gorm:"default:current_timestamp"`
+
+	User     	User     	`gorm:"foreignKey:UserID"`
+	Category 	Category	`gorm:"foreignKey:CategoryID"`
 }
