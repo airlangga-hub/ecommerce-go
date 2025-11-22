85abe00eee529bf002802454af710a3e9d427e4e
Airlangga <airlanggakrishna@gmail.com>
2025-11-23 02:29:03 +0700

    add explicit foreign key in bank account domain

--- internal/domain/BankAccount.go
+++ internal/domain/BankAccount.go
@@ -6,11 +6,13 @@ import (
 
 
 type BankAccount struct {
-	ID 					uint 		`json:"id" gorm:"PrimaryKey"`
+	ID 					uint 		`json:"id" gorm:"primaryKey"`
 	UserID 				uint 		`json:"user_id"`
 	BankAccountNumber 	uint 		`json:"bank_account_number" gorm:"index;unique;not null"`
 	SwiftCode 			string 		`json:"swift_code"`
 	PaymentType 		string 		`json:"payment_type"`
 	CreatedAt			time.Time	`json:"created_at" gorm:"default:current_timestamp"`
 	UpdatedAt			time.Time	`json:"updated_at" gorm:"default:current_timestamp"`
+	
+	User				User		`gorm:"foreignKey:UserID"`
 }
