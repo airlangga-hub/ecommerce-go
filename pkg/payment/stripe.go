6147f743e61f872b7d562d5d8241ada3aa20fbad
Airlangga <airlanggakrishna@gmail.com>
2025-11-27 14:13:50 +0700

    finished stripe.CreatePayment

--- pkg/payment/stripe.go
+++ pkg/payment/stripe.go
@@ -1,6 +1,13 @@
 package payment
 
-import "github.com/stripe/stripe-go/v78"
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/stripe/stripe-go/v78"
+	"github.com/stripe/stripe-go/v78/checkout/session"
+)
 
 
 type PaymentClient interface {
@@ -10,22 +17,56 @@ type PaymentClient interface {
 
 
 type payment struct {
-	StripeSecretKey		string
-	SuccessURL			string
-	FailureURL			string
+	stripeSecretKey		string
+	successURL			string
+	cancelURL			string
 }
 
 
-func NewPaymentClient(stripeSecretKey, successURL, failureURL string) PaymentClient {
+func NewPaymentClient(stripeSecretKey, successURL, cancelURL string) PaymentClient {
 	return &payment{
-		StripeSecretKey: stripeSecretKey,
-		SuccessURL: successURL,
-		FailureURL: failureURL,
+		stripeSecretKey: stripeSecretKey,
+		successURL: successURL,
+		cancelURL: cancelURL,
 	}
 }
 
 
-func (p *payment) CreatePayment(amount float64, userID, orderID uint) (*stripe.CheckoutSession, error)
+func (p *payment) CreatePayment(amount float64, userID, orderID uint) (*stripe.CheckoutSession, error) {
+	
+	amountInCents := amount * 100
+	
+	params := &stripe.CheckoutSessionParams{
+		PaymentMethodTypes: stripe.StringSlice([]string{"card"}),
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			{
+				PriceData: &stripe.CheckoutSessionLineItemPriceDataParams{
+					UnitAmount: stripe.Int64(int64(amountInCents)),
+					Currency: stripe.String("usd"),
+					ProductData: &stripe.CheckoutSessionLineItemPriceDataProductDataParams{
+						Name: stripe.String("Electronic"),
+					},
+				},
+				Quantity: stripe.Int64(1),
+			}, 
+		},
+		Mode: stripe.String(string(stripe.CheckoutSessionModePayment)),
+		SuccessURL: stripe.String(p.successURL),
+		CancelURL: stripe.String(p.cancelURL),
+	}
+	
+	params.AddMetadata("user_id", fmt.Sprintf("%d", userID))
+	params.AddMetadata("order_id", fmt.Sprintf("%d", orderID))
+	
+	session, err := session.New(params)
+	
+	if err != nil {
+		log.Println("--> stripe_err CreatePayment: ", err)
+		return nil, errors.New("error creating payment session")
+	}
+	
+	return session, nil
+}
 
 
 func (p *payment) GetPaymentStatus(paymentID string) (*stripe.CheckoutSession, error)
