64c36c8e75247550250bb11c2f32e0833fdb42df
Airlangga <airlanggakrishna@gmail.com>
2025-11-28 22:55:15 +0700

    add PaymentStatus type

--- internal/domain/Payment.go
+++ internal/domain/Payment.go
@@ -4,16 +4,27 @@ import "time"
 
 
 type Payment struct {
-	ID				uint		`json:"id" gorm:"primaryKey"`
-	UserID			uint		`json:"user_id"`
-	User			User		`json:"-"`
-	CaptureMethod	string		`json:"capture_method"`
-	Amount			float64		`json:"amount"`
-	TransactionID	uint		`json:"transaction_id"`
-	CustomerID		string		`json:"customer_id"`
-	PaymentID		string		`json:"payment_id"`
-	Status			string		`json:"status"`
-	Response		string		`json:"response"`
-	CreatedAt		time.Time	`json:"created_at"`
-	UpdatedAt		time.Time	`json:"updated_at"`
+	ID				uint				`json:"id" gorm:"primaryKey"`
+	UserID			uint				`json:"user_id"`
+	User			User				`json:"-"`
+	CaptureMethod	string				`json:"capture_method"`
+	Amount			float64				`json:"amount"`
+	TransactionID	uint				`json:"transaction_id"`
+	CustomerID		string				`json:"customer_id"`
+	PaymentID		string				`json:"payment_id"`
+	Status			PaymentStatus		`json:"status"`
+	Response		string				`json:"response"`
+	CreatedAt		time.Time			`json:"created_at"`
+	UpdatedAt		time.Time			`json:"updated_at"`
 }
+
+
+type PaymentStatus string
+
+
+const (
+	PaymentStatusInitial PaymentStatus = "initial"
+	PaymentStatusSuccess PaymentStatus = "success"
+	PaymentStatusFailed PaymentStatus = "failed"
+	PaymentStatusPending PaymentStatus = "pending"
+)
